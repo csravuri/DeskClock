f5c2812a5cebd812d9a6a7dd291eaaa1b1af1bf9
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-07-30 22:33:47 +0530

    Initial app

---
+++ DeskClock/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DeskClock;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DeskClock/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DeskClock;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
