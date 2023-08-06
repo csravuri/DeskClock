9344ca68a853633a09543ec514770fb7f5be7a34
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-08-06 13:54:22 +0530

    Timer UI view model, inspired from stopwatch

--- DeskClock/ViewModels/TimerViewModel.cs
+++ DeskClock/ViewModels/TimerViewModel.cs
@@ -6,13 +6,68 @@ namespace DeskClock.ViewModels
 {
 	public partial class TimerViewModel : ObservableObject
 	{
+		public TimerViewModel(IDispatcher dispatcher)
+		{
+			dispatcherTimer = dispatcher.CreateTimer();
+			dispatcherTimer.Interval = Time1Second;
+			UpdateTimer();
+		}
+
 		[ObservableProperty]
-		TimeSpan selectedTime;
+		string timerNow;
+
+		[ObservableProperty]
+		bool isRunning;
+
+		[RelayCommand]
+		void Start()
+		{
+			IsRunning = true;
+			dispatcherTimer.Tick += DispatcherTimer_Tick;
+			dispatcherTimer.Start();
+		}
+
+		[RelayCommand]
+		void Pause()
+		{
+			IsRunning = false;
+			dispatcherTimer.Tick -= DispatcherTimer_Tick;
+			dispatcherTimer.Stop();
+		}
+
+		[RelayCommand]
+		void Reset()
+		{
+			timeCounter = TimeSpan.Zero;
+			UpdateTimer();
+		}
 
 		[RelayCommand]
 		async Task EditTime()
 		{
 			await Shell.Current.GoToAsync(nameof(TimeSelectionPage));
 		}
+
+		void DispatcherTimer_Tick(object sender, EventArgs e)
+		{
+			if (timeCounter <= TimeSpan.Zero)
+			{
+				Pause();
+				Reset();
+				return;
+			}
+			timeCounter -= Time1Second;
+			UpdateTimer();
+		}
+
+		void UpdateTimer()
+		{
+			TimerNow = string.Format("{0}:{1:00}:{2:00}",
+				(int)timeCounter.TotalHours, timeCounter.Minutes, timeCounter.Seconds);
+		}
+
+		readonly TimeSpan Time1Second = TimeSpan.FromSeconds(1);
+		TimeSpan timeCounter = TimeSpan.Zero;
+		private readonly IDispatcherTimer dispatcherTimer;
 	}
 }
